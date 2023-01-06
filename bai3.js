77c6bdb6e8672df383a35f85da64b5a29eba8698
nguyennhattan128 <nguyennhattanjp@gmail.com>
2023-01-06 16:15:44 +0700

    bai ve js

---
+++ bai3.js
@@ -0,0 +1,13 @@
+let a = prompt('nhâp số a');
+let b = prompt('nhập số b');
+parseInt(a);
+ parseInt(b);
+
+
+if (a%b===0) {
+    document.write( a +' la boi so cua ' + b );
+}
+else{
+    document.write(a + ' khong la boi so cua ' + b);
+
+}
