29a1bb38c197d365139f7fb4a324d8a0eabf030b
nguyennhattan128 <nguyennhattanjp@gmail.com>
2023-01-06 16:38:37 +0700

    dien tich hinh chu nhat

---
+++ sudungtoantu/sudungtoantu1.js
@@ -0,0 +1,6 @@
+let width = prompt('enter the width');
+let height =prompt('enter the height');
+parseInt(  width);
+parseInt(height);
+let area = width * height;
+document.write(area  + ' la dien tich hinh chu nhat ');
